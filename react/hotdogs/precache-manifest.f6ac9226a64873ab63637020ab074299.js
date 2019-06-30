9e668efb3641a00d546078ed1d8438f96b0453f7
dmitriy10 <dmitriy10@ukr.net>
2019-06-30 15:41:16 +0300

    react hotdogs added

---
+++ react/hotdogs/precache-manifest.f6ac9226a64873ab63637020ab074299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a244c1a204cf7743a5fcd9364d1ea0ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada3d5bf80fab10c02f2",
+    "url": "/static/css/main.781ca1c7.chunk.css"
+  },
+  {
+    "revision": "05f4949ded5a8b22d684",
+    "url": "/static/js/2.511b5c7d.chunk.js"
+  },
+  {
+    "revision": "ada3d5bf80fab10c02f2",
+    "url": "/static/js/main.0f1cc15c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
