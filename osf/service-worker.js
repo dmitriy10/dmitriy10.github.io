60d86fdeec27a59dc6dab5c71eac0a51165595b0
dmitriy10 <dmitriy10@ukr.net>
2019-09-26 00:21:09 +0300

    osf some changes

--- osf/service-worker.js
+++ osf/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bb25f2d09f62981a9bea2bd39a52a31.js"
+  "precache-manifest.0bb25f2d09f62981a9bea2bd39a52a31.js"
 );
 
 self.addEventListener('message', (event) => {
