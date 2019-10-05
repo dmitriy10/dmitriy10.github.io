12401d0aad3cb66101cd1220acbe320db0ca631d
dmitriy10 <dmitriy10@ukr.net>
2019-10-05 19:20:47 +0300

    osf

---
+++ osf/precache-manifest.1416513bf63f49a5d4697eb9902fe19b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8676052b3bb4e087617aa129c150b681",
+    "url": "/osf/index.html"
+  },
+  {
+    "revision": "2e637f2089453b9d9641",
+    "url": "/osf/static/css/2.70b0fc90.chunk.css"
+  },
+  {
+    "revision": "703bdeaf6a21c1a460b4",
+    "url": "/osf/static/css/main.565f9142.chunk.css"
+  },
+  {
+    "revision": "2e637f2089453b9d9641",
+    "url": "/osf/static/js/2.d477a7de.chunk.js"
+  },
+  {
+    "revision": "703bdeaf6a21c1a460b4",
+    "url": "/osf/static/js/main.4c1cbb6e.chunk.js"
+  },
+  {
+    "revision": "516ed15ad347fc07c045",
+    "url": "/osf/static/js/runtime~main.737b849e.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/osf/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/osf/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/osf/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/osf/static/media/slick.f97e3bbf.svg"
+  }
+]);
